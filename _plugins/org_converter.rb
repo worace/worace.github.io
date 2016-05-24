904eb7dc7550c685b3b2cc39a125983b834d5660
Horace Williams <horace.d.williams@gmail.com>
2016-05-23 19:01:39 -0600

    add org plugin and use Gemfile rather than _config.yml for setting gems

---
+++ _plugins/org_converter.rb
@@ -0,0 +1,25 @@
+require 'org-ruby'
+
+module Jekyll
+  # Convert org-mode files.
+  class OrgConverter < Converter
+    safe true
+
+    def setup
+      # No-op
+    end
+
+    def matches(ext)
+      ext =~ /org$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      setup
+      Orgmode::Parser.new(content).to_html
+    end
+  end
+end
