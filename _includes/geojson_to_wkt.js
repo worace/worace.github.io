fce3dd4e000f3ce8465064a3962be0883b696bd4
Horace Williams <horace.d.williams@gmail.com>
2018-04-05 14:29:27 -0700

    Add copy to clipboard, paste detection, and geom collection support

--- _includes/geojson_to_wkt.js
+++ _includes/geojson_to_wkt.js
@@ -1,27 +1,61 @@
+function wkt(geojson) {
+  if (geojson.type === "FeatureCollection") {
+    const wkts = geojson.features.map(wellknown.stringify);
+    return "GEOMETRYCOLLECTION (" + wkts.join(",") + ")";
+  } else {
+    return wellknown.stringify(geojson);
+  }
+}
+function convertGeoJSON() {
+  const rawJSON = $("#geojson-input").val();
+
+  const geojson = JSON.parse(rawJSON);
+  const pretty = JSON.stringify(geojson, null, 2)
+
+  $("#geojson-input").val(pretty);
+
+  $("#wkt-output").text(wkt(geojson));
+}
+
+function convertWKT() {
+  const rawWKT = $.trim($("#wkt-input").val());
+
+  $("#wkt-input").val(rawWKT);
+  const geojson = wellknown.parse(rawWKT);
+  $("#geojson-output").text(JSON.stringify(geojson));
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
 $(document).ready(() => {
   $("#geojson-form").submit((e) => {
     e.preventDefault();
-    console.log(e);
-    const rawJSON = $("#geojson-input").val();
-    const geojson = JSON.parse(rawJSON);
-    const pretty = JSON.stringify(geojson, null, 2)
-    $("#geojson-input").val(pretty);
-    const wkt = wellknown.stringify(geojson);
-
-    console.log('set wkt: ', wkt);
-    $("#wkt-output").text(wkt);
+    convertGeoJSON();
   });
 
   $("#wkt-form").submit((e) => {
-    console.log('submitted');
     e.preventDefault();
-    console.log(e);
-    const rawWKT = $.trim($("#wkt-input").val());
-    $("#wkt-input").val(rawWKT);
-    console.log(rawWKT);
-    const geojson = wellknown.parse(rawWKT);
-    console.log(geojson);
+    convertWKT();
+  });
 
-    $("#geojson-output").text(JSON.stringify(geojson));
+  $("#geojson-input").on('paste', (e) => {
+    // const text = e.originalEvent.clipboardData.getData('text');
+    setTimeout(convertGeoJSON, 100);
+  });
+  $("#wkt-input").on('paste', (e) => {
+    setTimeout(convertWKT, 100);
+  });
+
+  $("#copy-wkt").on('click', (e) => {
+    copyToClipboard("#wkt-output");
+  });
+  $("#copy-geojson").on('click', (e) => {
+    copyToClipboard("#geojson-output");
   });
 });
