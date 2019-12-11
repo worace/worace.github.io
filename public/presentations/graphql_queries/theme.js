ff89bd71ba16b4d80f2a44d6b472cf42ca42dc3a
Horace Williams <horace.d.williams@gmail.com>
2019-12-10 17:16:50 -0800

    Theming work

---
+++ public/presentations/graphql_queries/theme.js
@@ -0,0 +1,38 @@
+const white = '#fffceb'
+const black = '#11111f'
+const blue = '#2d5dd7'
+
+export default {
+  fonts: {
+    body: '"Open Sans", sans-serif',
+  },
+  googleFont: 'https://fonts.googleapis.com/css?family=Open+Sans',
+  colors: {
+    text: '#073642',
+    background: '#fdf6e3',
+    link: '#268bd2',
+  },
+  styles: {
+    root: {
+      textAlign: 'left',
+      fontSize: ['1.5em', '3em'],
+      code: {
+        background: '#fdf6e3'
+      }
+    },
+    pre: {
+      background: '#fdf6e3',
+      code: {
+        background: '#fdf6e3'
+      }
+    },
+    code: {
+      background: '#fdf6e3'
+    },
+    Slide: {
+      display: 'block',
+      padding: '2em',
+      textAlign: 'left',
+    },
+  },
+}
