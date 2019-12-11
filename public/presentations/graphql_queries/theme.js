ee84ea41eaf4ced45a1efd3954829cd9201ebbed
Horace Williams <horace.d.williams@gmail.com>
2019-12-10 21:46:47 -0800

    Hopefully final slides + theming

--- public/presentations/graphql_queries/theme.js
+++ public/presentations/graphql_queries/theme.js
@@ -2,32 +2,51 @@ const white = '#fffceb'
 const black = '#11111f'
 const blue = '#2d5dd7'
 
+const sdBG = '#073642';
+const sdText = '#93a1a1';
+const sdLink = '#dc322f';
+
+const slBG = '#fdf6e3';
+const slText = '073642';
+const slLink = '#268bd2';
+
+// const colors = {
+//   background: sdBG,
+//   text: slBG,
+//   link: sdLink
+// };
+
+const colors = {
+  background: slBG,
+  text: slText,
+  link: slLink
+};
+
 export default {
   fonts: {
     body: '"Open Sans", sans-serif',
   },
   googleFont: 'https://fonts.googleapis.com/css?family=Open+Sans',
-  colors: {
-    text: '#073642',
-    background: '#fdf6e3',
-    link: '#268bd2',
-  },
+  colors,
+  fontSizes: [
+  //b        6   5   4   3  h2+code  h1
+    12, 14, 18, 20, 20, 24, 28, 36
+  ],
   styles: {
     root: {
       textAlign: 'left',
-      fontSize: ['1.5em', '3em'],
       code: {
-        background: '#fdf6e3'
+        background: colors.background
       }
     },
     pre: {
-      background: '#fdf6e3',
+      background: colors.background,
       code: {
-        background: '#fdf6e3'
+        background: colors.background
       }
     },
     code: {
-      background: '#fdf6e3'
+      background: colors.background
     },
     Slide: {
       display: 'block',
